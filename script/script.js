9d8c294b2e0fb9aba75005987a48fb9d1d877a11
Legend-1409 <yashbansal1409@gmail.com>
2021-11-23 10:54:10 +0530

    added image clicking effect

--- script/script.js
+++ script/script.js
@@ -4,4 +4,20 @@ likeButton.forEach(element => {
     element.addEventListener('click', () => {
         element.classList.toggle('is-liked');
     });
-})
+});
+
+let imageModal = document.querySelector('#myModal');
+let allImages = document.querySelectorAll('.card-image figure img');
+let modalContent = document.querySelector('#img01');
+let imageCloseButton = document.querySelector('#myModal .close');
+
+allImages.forEach(element => {
+    element.addEventListener('click', () => {
+        imageModal.classList.add('is-active');
+        modalContent.src = element.src;
+    });
+});
+
+imageCloseButton.addEventListener('click', () => {
+    imageModal.classList.remove('is-active');
+});
